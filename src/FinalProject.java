6eb5432416773153fd628603be860485ac8c5e87
KANGH4484 <KANGH4484@HRH-IC0027337.wrdsb.ca>
2016-05-30 08:54:30 -0400

    d1

---
+++ src/FinalProject.java
@@ -0,0 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author kangh4484
+ */
+
+
+public class FinalProject extends JComponent implements KeyListener {
+
+    // Height and Width of our game
+    static final int WIDTH = 600;
+    static final int HEIGHT = 800;
+    
+    // sets the framerate and delay for our game
+    // you just need to select an approproate framerate
+    long desiredFPS = 60;
+    long desiredTime = (1000)/desiredFPS;
+    
+    int speed = 2;
+    
+    // drawing of the game happens in here
+    // we use the Graphics object, g, to perform the drawing
+    // NOTE: This is already double buffered!(helps with framerate/speed)
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        // always clear the screen first!
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+        
+        // GAME DRAWING GOES HERE 
+        g.setColor(Color.CYAN);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        
+        g.setColor(Color.BLACK);
+        g.fillRect(100, 50, WIDTH-150, HEIGHT-100);
+        // GAME DRAWING ENDS HERE
+    }
+    
+    
+    // The main game loop
+    // In here is where all the logic for my game will go
+    public void run()
+    {
+        // Used to keep track of time used to draw and update the game
+        // This is used to limit the framerate later on
+        long startTime;
+        long deltaTime;
+        
+        // the main game loop section
+        // game will end if you set done = false;
+        boolean done = false; 
+        while(!done)
+        {
+            // determines when we started so we can keep a framerate
+            startTime = System.currentTimeMillis();
+            
+            // all your game rules and move is done in here
+            // GAME LOGIC STARTS HERE 
+            
+            
+
+            // GAME LOGIC ENDS HERE 
+            
+            // update the drawing (calls paintComponent)
+            repaint();
+            
+            
+            
+            // SLOWS DOWN THE GAME BASED ON THE FRAMERATE ABOVE
+            // USING SOME SIMPLE MATH
+            deltaTime = System.currentTimeMillis() - startTime;
+            if(deltaTime > desiredTime)
+            {
+                //took too much time, don't wait
+            }else
+            {
+                try
+                {
+                    Thread.sleep(desiredTime - deltaTime);
+                }catch(Exception e){};
+            }
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creates a windows to show my game
+        JFrame frame = new JFrame("My Game");
+       
+        // creates an instance of my game
+        FinalProject game = new FinalProject();
+        // sets the size of my game
+        game.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        // adds the game to the window
+        frame.add(game);
+         
+        // sets some options and size of the window automatically
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        // shows the window to the user
+        frame.setVisible(true);
+        //make the game listen for keyboard
+        frame.addKeyListener(game);
+        // starts my game loop
+        game.run();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        }
+}
